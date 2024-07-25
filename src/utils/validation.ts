4380748bcc1157cfe52f03324facf4a423cbcdc1
sohyeonAn <ansh4393@gmail.com>
2024-07-25 23:30:24 +0900

    refactor: 필드별 검증 기능을 검증방법별 검증으로 변경

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,28 +1,26 @@
 import { FormValues as LoginFormValues } from '@/types/auth/login'
 import { FormValues as SignupFormValues } from '@/types/auth/signup'
 
-export function validateEmail(email: string) {
-  const emailValidRegex =
-    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/
-
-  return emailValidRegex.test(email)
+export function validateRegex(value: string, regex: RegExp) {
+  return regex.test(value)
 }
 
-export function validatePassword(password: string) {
-  return password.length >= 8
-}
-
-export function validateName(name: string) {
-  return name.length >= 2
+export function validateStringMinLength(value: string, minLength: number) {
+  return value.length >= minLength
 }
 
 export function validateLoginForm(formValues: LoginFormValues) {
   let errors: Partial<LoginFormValues> = {}
 
-  if (!validateEmail(formValues.email)) {
+  if (
+    !validateRegex(
+      formValues.email,
+      /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
+    )
+  ) {
     errors.email = '이메일 형식을 확인해주세요'
   }
-  if (!validatePassword(formValues.password)) {
+  if (!validateStringMinLength(formValues.password, 8)) {
     errors.password = '비밀번호를 8글자 이상 입력해주세요'
   }
 
@@ -32,20 +30,25 @@ export function validateLoginForm(formValues: LoginFormValues) {
 export function validateSignupForm(formValues: SignupFormValues) {
   let errors: Partial<SignupFormValues> = {}
 
-  if (!validateEmail(formValues.email)) {
+  if (
+    !validateRegex(
+      formValues.email,
+      /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
+    )
+  ) {
     errors.email = '이메일 형식을 확인해주세요'
   }
-  if (!validatePassword(formValues.password)) {
+  if (!validateStringMinLength(formValues.password, 8)) {
     errors.password = '비밀번호를 8글자 이상 입력해주세요'
   }
 
-  if (!validatePassword(formValues.passwordConfirm)) {
+  if (!validateStringMinLength(formValues.passwordConfirm, 8)) {
     errors.passwordConfirm = '비밀번호를 8글자 이상 입력해주세요'
   } else if (formValues.password !== formValues.passwordConfirm) {
     errors.passwordConfirm = '비밀번호가 일치하지 않습니다'
   }
 
-  if (!validateName(formValues.name)) {
+  if (!validateStringMinLength(formValues.name, 2)) {
     errors.name = '이름을 2글자 이상 입력해주세요'
   }
 
