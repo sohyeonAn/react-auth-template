39bb48e8fa6b22946fb49f5b4072e8609fcb1030
sohyeonAn <ansh4393@gmail.com>
2024-07-28 22:11:09 +0900

    refactor: SignupForm을 zod 사용하여 검증하도록 변경

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -2,14 +2,36 @@ import { FormValues } from '@/types/auth/signup'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { INVALIDATION_MESSAGES, VALIDATION_RULES } from '@/constants/validation'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z
+  .object({
+    email: z.string().regex(VALIDATION_RULES.email.pattern, {
+      message: INVALIDATION_MESSAGES.email.pattern,
+    }),
+    password: z.string().min(VALIDATION_RULES.password.minLength, {
+      message: INVALIDATION_MESSAGES.password.minLength,
+    }),
+    passwordConfirm: z.string().min(VALIDATION_RULES.password.minLength, {
+      message: INVALIDATION_MESSAGES.password.minLength,
+    }),
+    name: z.string().min(VALIDATION_RULES.name.minLength, {
+      message: INVALIDATION_MESSAGES.name.minLength,
+    }),
+  })
+  .required()
+  .refine((data) => data.password === data.passwordConfirm, {
+    path: ['passwordConfirm'],
+    message: INVALIDATION_MESSAGES.passwordConfirm.match,
+  })
 
 export default function SignupForm() {
   const {
     register,
     handleSubmit,
-    getValues,
     formState: { errors, isValid },
-  } = useForm<FormValues>({ mode: 'onChange' })
+  } = useForm<FormValues>({ mode: 'onChange', resolver: zodResolver(schema) })
 
   const onSubmit = (formValues: FormValues) => {
     //   @TODO: 로그인 처리
@@ -21,32 +43,12 @@ export default function SignupForm() {
       <h1>회원가입</h1>
       <div>
         <label htmlFor="email">이메일</label>
-        <input
-          type="email"
-          id="email"
-          {...register('email', {
-            pattern: {
-              value: VALIDATION_RULES.email.pattern,
-              message: INVALIDATION_MESSAGES.email.pattern,
-            },
-            required: true,
-          })}
-        />
+        <input type="email" id="email" {...register('email')} />
         {errors.email && <p>{errors.email.message}</p>}
       </div>
       <div>
         <label htmlFor="password">비밀번호</label>
-        <input
-          type="password"
-          id="password"
-          {...register('password', {
-            minLength: {
-              value: VALIDATION_RULES.password.minLength,
-              message: INVALIDATION_MESSAGES.password.minLength,
-            },
-            required: true,
-          })}
-        />
+        <input type="password" id="password" {...register('password')} />
         {errors.password && <p>{errors.password.message}</p>}
       </div>
       <div>
@@ -54,35 +56,13 @@ export default function SignupForm() {
         <input
           type="password"
           id="passwordConfirm"
-          {...register('passwordConfirm', {
-            minLength: {
-              value: VALIDATION_RULES.password.minLength,
-              message: INVALIDATION_MESSAGES.password.minLength,
-            },
-            validate: (value) => {
-              return (
-                value === getValues('password') ||
-                INVALIDATION_MESSAGES.passwordConfirm.match
-              )
-            },
-            required: true,
-          })}
+          {...register('passwordConfirm')}
         />
         {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
       </div>
       <div>
         <label htmlFor="name">이름</label>
-        <input
-          type="text"
-          id="name"
-          {...register('name', {
-            minLength: {
-              value: VALIDATION_RULES.name.minLength,
-              message: INVALIDATION_MESSAGES.name.minLength,
-            },
-            required: true,
-          })}
-        />
+        <input type="text" id="name" {...register('name')} />
         {errors.name && <p>{errors.name.message}</p>}
       </div>
 
