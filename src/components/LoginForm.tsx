9f247449dbeb8313da02ed9521242637a9f63b06
sohyeonAn <ansh4393@gmail.com>
2024-07-28 22:04:36 +0900

    refactor: LoginForm을 zod 사용하여 검증하도록 변경

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,13 +2,26 @@ import { FormValues } from '@/types/auth/login'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { INVALIDATION_MESSAGES, VALIDATION_RULES } from '@/constants/validation'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+  email: z.string().regex(VALIDATION_RULES.email.pattern, {
+    message: INVALIDATION_MESSAGES.email.pattern,
+  }),
+  password: z
+    .string()
+    .min(VALIDATION_RULES.password.minLength, {
+      message: INVALIDATION_MESSAGES.password.minLength,
+    }),
+})
 
 export default function LoginForm() {
   const {
     register,
     handleSubmit,
     formState: { isValid },
-  } = useForm<FormValues>()
+  } = useForm<FormValues>({ resolver: zodResolver(schema) })
 
   const onSubmit = (formValues: FormValues) => {
     //   @TODO: 로그인 처리
@@ -20,31 +33,11 @@ export default function LoginForm() {
       <h1>로그인</h1>
       <div>
         <label htmlFor="email">이메일</label>
-        <input
-          type="email"
-          id="email"
-          {...register('email', {
-            pattern: {
-              value: VALIDATION_RULES.email.pattern,
-              message: INVALIDATION_MESSAGES.email.pattern,
-            },
-            required: true,
-          })}
-        />
+        <input type="email" id="email" {...register('email')} />
       </div>
       <div>
         <label htmlFor="password">비밀번호</label>
-        <input
-          type="password"
-          id="password"
-          {...register('password', {
-            minLength: {
-              value: VALIDATION_RULES.password.minLength,
-              message: INVALIDATION_MESSAGES.password.minLength,
-            },
-            required: true,
-          })}
-        />
+        <input type="password" id="password" {...register('password')} />
       </div>
       <button type="submit" disabled={isValid === false}>
         로그인
